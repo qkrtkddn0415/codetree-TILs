6bc138ec43ef3417a5de7f38f42c25bf4846607b
Sangwoo <79515593+qkrtkddn0415@users.noreply.github.com>
2024-03-01 01:27:00 +0900

    Title: 개발자의 가위바위보, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -0,0 +1,10 @@
+N=int(input())
+a_lst, b_lst= [0]*4, [0]*4
+for _ in range(N):
+    a, b = map(int,input().split())
+    a_lst[a]+=1
+    b_lst[b]+=1
+mx=0
+for i in range(1,4):
+    mx=max(a_lst[i]-b_lst[i],mx)
+print(mx)
