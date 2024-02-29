a28aa75db996f9e33ab1f36b5da628c7bee44de1
Sangwoo <79515593+qkrtkddn0415@users.noreply.github.com>
2024-03-01 02:12:02 +0900

    Title: 연속된 숫자 만들기 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240301/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -2,7 +2,8 @@ lst=list(map(int,input().split()))
 lst.sort()
 if lst[0]+1==lst[1] and lst[1]+1==lst[2]:
     print('0')
-mx=0
-mx=max(mx,lst[2]-lst[1]-1)
-mx=max(mx,lst[1]-lst[0]-1)
-print(mx)
+else:
+    mx=0
+    mx=max(mx,lst[2]-lst[1]-1)
+    mx=max(mx,lst[1]-lst[0]-1)
+    print(mx)
